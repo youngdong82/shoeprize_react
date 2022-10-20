1fd131eaa7b97771bb210ae358522033a008e681
youngdong82 <nfs82young@gmail.com>
2022-10-20 15:58:10 +0900

    DESIGN: Create style in ThumbnailList
    ThumbnailList에 스타일 추가

--- src/components/ThumbnailList.js
+++ src/components/ThumbnailList.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import style from 'styled-components';
 
 const ThumbnailList = () => {
+  const viewportWidth = window.innerWidth;
+
   return(
-    <main>
-      ThumbnailList
-    </main>
+    <ThumbnailListComp width={viewportWidth}>
+      <Img width={viewportWidth} src={null} alt="detail image" />
+      <Img width={viewportWidth} src={null} alt="detail image" />
+      <Img width={viewportWidth} src={null} alt="detail image" />
+      <Img width={viewportWidth} src={null} alt="detail image" />
+      <Img width={viewportWidth} src={null} alt="detail image" />
+      <Img width={viewportWidth} src={null} alt="detail image" />
+    </ThumbnailListComp>
   )
 }
 
 export default ThumbnailList;
+
+const ThumbnailListComp = style.main`
+  width: ${({width}) => width/1920 * 1220}px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: ${({width}) => width/1920 * 96}px;
+  margin-left: ${({width}) => width/1920 * 40}px;
+  background-color: beige;
+`
+const Img = style.img`
+  width: ${({width}) => width/1920 * 600}px;
+  height: ${({width}) => width/1920 * 600}px;
+`
