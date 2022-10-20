4b4c847a2545ffc06b272461cea08a5151bb7b22
youngdong82 <nfs82young@gmail.com>
2022-10-20 19:36:05 +0900

    FEAT: Add Style in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import style from 'styled-components'
 
 const Header = () => {
+  const viewportWidth = window.innerWidth;
+
   return(
-    <header>
-      Header
-    </header>
+    <HeaderComp width={viewportWidth}>
+      <LogoImg src={null} alt='logo' />
+    </HeaderComp>
   )
 }
 
 export default Header;
+
+const HeaderComp = style.header`
+  width: 100vw;
+  height: ${({width}) => width/1920 * 56}px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 40px;
+  border-bottom: 1px solid black;
+`
+const LogoImg = style.img`
+  width: ${({width}) => width/1920 * 116}px;
+`
