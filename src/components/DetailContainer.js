c371ab3164506286b65e2fb95f0384437412e858
youngdong82 <nfs82young@gmail.com>
2022-10-20 19:25:38 +0900

    FEAT: Add style in DetailContainer_2

--- src/components/DetailContainer.js
+++ src/components/DetailContainer.js
@@ -6,15 +6,20 @@ const DetailContainer = () => {
   const viewportWidth = window.innerWidth;
   return(
     <DetailContainerComp width={viewportWidth}>
-      <DetailTitle>
-        DetailTitle
-      </DetailTitle>
-      <DetailData>
-        DetailData
-      </DetailData>
-      <DetailDesc>
-        DetailDesc
-      </DetailDesc>
+      <DetailHead>
+        <DetailTitle>
+          DetailTitle
+        </DetailTitle>
+        <DetailData>
+          DetailData
+        </DetailData>
+        <DetailDesc>
+          DetailDesc
+        </DetailDesc>
+        <DetailElse>
+          DetailElse
+        </DetailElse>
+      </DetailHead>
       <Sep />
       <ReleaseList />
     </DetailContainerComp>
@@ -30,25 +35,42 @@ const DetailContainerComp = style.aside`
   align-items: center;
   margin-top: ${({width}) => width/1920 * 216}px;
 `
-const DetailTitle = style.section`
+const DetailHead = style.section`
   width: ${({width}) => width/1920 * 400}px;
   min-width: 400px;
+  display: flex;
+  flex-direction: column;
+`
+const DetailTitle = style.article`
+  width: 100%;
   height: 62px;
+  margin-bottom: 12px;
   background-color: red;
 `
-const DetailData = style.section`
-  width: ${({width}) => width/1920 * 400}px;
+const DetailData = style.article`
+  width: 100%;
   min-width: 400px;
   height: 80px;
+  margin-bottom: 12px;
+  background-color: red;
 `
-const DetailDesc = style.section`
-  width: ${({width}) => width/1920 * 400}px;
+const DetailDesc = style.article`
+  width: 100%;
   min-width: 400px;
   height: 62px;
+  margin-bottom: 16px;
+  background-color: red;
+`
+const DetailElse = style.div`
+  width: 100%;
+  min-width: 400px;
+  height: 16px;
+  margin-bottom: 24px;
 `
 const Sep = style.div`
-  width: ${({width}) => width/1920 * 400}px;
+  width: 400px;
   min-width: 400px;
   height: 1px;
   background-color: black;
+  margin-bottom: 28px;
 `
